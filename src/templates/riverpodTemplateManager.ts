80792b9cf20a60daf79bcd8ee95f6f57274afa45
nal-minhnt3 <minhnt3@nal.vn>
2025-06-04 12:41:28 +0700

    Revert "Remove unused riverpod templates"
    This reverts commit ff9d73c081acacc9ad2c1a5d66b75ebbad9e5b7e.

--- src/templates/riverpodTemplateManager.ts
+++ src/templates/riverpodTemplateManager.ts
@@ -4,18 +4,30 @@ export interface RiverpodTemplates {
   getStateTemplate: (feature: string) => string;
 }
 
-import { getPageTemplate } from "./riverpod_page.template";
-import { getViewModelTemplate } from "./view_model.template";
-import { getStateTemplate } from "./state.template";
+import { getPageTemplate as getPageTemplate1 } from "./riverpod_page.template";
+import { getViewModelTemplate as getViewModelTemplate1 } from "./view_model.template";
+import { getStateTemplate as getStateTemplate1 } from "./state.template";
+
+import { getPageTemplate as getPageTemplate2 } from "./riverpod_page.template_2";
+import { getViewModelTemplate as getViewModelTemplate2 } from "./view_model.template_2";
+import { getStateTemplate as getStateTemplate2 } from "./state.template_2";
 
 const templates: { [key: string]: RiverpodTemplates } = {
   template1: {
-    getPageTemplate,
-    getViewModelTemplate,
-    getStateTemplate,
+    getPageTemplate: getPageTemplate1,
+    getViewModelTemplate: getViewModelTemplate1,
+    getStateTemplate: getStateTemplate1,
+  },
+  template2: {
+    getPageTemplate: getPageTemplate2,
+    getViewModelTemplate: getViewModelTemplate2,
+    getStateTemplate: getStateTemplate2,
   },
 };
 
-export function getRiverpodTemplates(_: string | undefined = undefined): RiverpodTemplates {
+export function getRiverpodTemplates(name: string | undefined): RiverpodTemplates {
+  if (name && templates[name]) {
+    return templates[name];
+  }
   return templates["template1"];
 }
